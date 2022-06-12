8efd9a1cb4b75e2ceac60119707328019a3ee0ee
priyankawaghmare18 <63039867+priyankawaghmare18@users.noreply.github.com>
2022-06-12 16:16:33 +0530

    src_2Que

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstCompComponent } from './first-comp/first-comp.component';
+import { SecCompComponent } from './sec-comp/sec-comp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstCompComponent
+    FirstCompComponent,
+    SecCompComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sec-comp/sec-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sec-comp',
+  templateUrl: './sec-comp.component.html',
+  styleUrls: ['./sec-comp.component.css']
+})
+export class SecCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
